dad65fbba36c14baf9cde18a8b312c897e884530
tNhanDerivative <tnhanderivative@zohomail.com>
2022-09-25 21:29:48 +0700

    add home path to urls.py and fix index.html

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -17,9 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index' )
 ]
 
 if settings.DEBUG:
